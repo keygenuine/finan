1c46a3bf856337e4bc05c48ab20d2b6c3241cb9b
douglas <dss.sacramentos@gmail.com>
2024-01-05 12:31:45 -0300

    ultimas mudanças

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const json = require('express')
 const app = express()
+const dot = require('dotenv/config')
+
+const port = process.env.PORT
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.post('/insere', (req,resp)=>{
     console.log(req.body)
     resp.send('ola')
 })
-app.listen(8081)
+app.listen(port || 5000, () => console.log(`Server running on port:${port} `));
+
+console.log(port)
